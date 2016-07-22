25d5cd2e15b79f714390ab8712323ce7589457ed
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-22 16:59:36 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+ var pTags = $('p')
+ return pTags
+}
+
+function lastImageSelector() {
+ return $('img:last')
+}
+
+function ninjaBabySelector() {
+ return $('#baby-ninja')
+}
+// <img id="baby-ninja" src="https://s3.amazonaws.com/after-school-assets/baby-ninja.gif" width="300px">
+
+function divSelector() {
+ return $('div')
+}
+
+function firstListItem() {
+ return $('ul li:first')
+}
